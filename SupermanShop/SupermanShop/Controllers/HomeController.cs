c8cd27c8f74361a35770f47c331f1450dfcf5ae9
JOHU098 <86375821+JOHU098@users.noreply.github.com>
2023-02-19 16:11:25 +0600

    Part 12

--- SupermanShop/SupermanShop/Controllers/HomeController.cs
+++ SupermanShop/SupermanShop/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using SupermanShop.Models.Home;
+﻿using Newtonsoft.Json;
+using SupermanShop.DAL;
+using SupermanShop.Models;
+using SupermanShop.Models.Home;
+using SupermanShop.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +13,38 @@ namespace SupermanShop.Controllers
 {
     public class HomeController : Controller
     {
+        dbSupermanShopEntities1 ctx = new dbSupermanShopEntities1();
         public ActionResult Index(String search,int? page)
         {
             HomeIndexViewModel model = new HomeIndexViewModel();
             return View(model.CreateModel(search,4,page));
+        }
+
+        public ActionResult AddToCart(int productId)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = ctx.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = ctx.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            return Redirect("Index");
         }
 
         public ActionResult About()
