763952be5c91c2d5f4b2635f6cacf463c721995d
JOHU098 <86375821+JOHU098@users.noreply.github.com>
2023-02-19 21:02:36 +0600

    Part 14, 15(Half)

--- SupermanShop/SupermanShop/Controllers/HomeController.cs
+++ SupermanShop/SupermanShop/Controllers/HomeController.cs
@@ -19,8 +19,42 @@ namespace SupermanShop.Controllers
             HomeIndexViewModel model = new HomeIndexViewModel();
             return View(model.CreateModel(search,4,page));
         }
-
-        public ActionResult AddToCart(int productId)
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult CheckoutDetails()
+        {
+            return View();
+        }
+        public ActionResult DecreaseQty(int productId)
+        {
+            if (Session["cart"] != null)
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = ctx.Tbl_Product.Find(productId);
+                foreach (var item in cart)
+                {
+                    if (item.Product.ProductId == productId)
+                    {
+                        int prevQty = item.Quantity;
+                        if (prevQty > 0)
+                        {
+                            cart.Remove(item);
+                            cart.Add(new Item()
+                            {
+                                Product = product,
+                                Quantity = prevQty - 1
+                            });
+                        }
+                        break;
+                    }
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect("Checkout");
+        }
+        public ActionResult AddToCart(int productId, string url)
         {
             if (Session["cart"] == null)
             {
@@ -36,29 +70,37 @@ namespace SupermanShop.Controllers
             else
             {
                 List<Item> cart = (List<Item>)Session["cart"];
+                var count = cart.Count();
                 var product = ctx.Tbl_Product.Find(productId);
-                cart.Add(new Item()
+                for (int i = 0; i < count; i++)
                 {
-                    Product = product,
-                    Quantity = 1
-                });
+                    if (cart[i].Product.ProductId == productId)
+                    {
+                        int prevQty = cart[i].Quantity;
+                        cart.Remove(cart[i]);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty + 1
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        var prd = cart.Where(x => x.Product.ProductId == productId).SingleOrDefault();
+                        if (prd == null)
+                        {
+                            cart.Add(new Item()
+                            {
+                                Product = product,
+                                Quantity = 1
+                            });
+                        }
+                    }
+                }
                 Session["cart"] = cart;
             }
-            return Redirect("Index");
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return Redirect(url);
         }
 
         public ActionResult RemoveFromCart(int productId)
@@ -74,6 +116,20 @@ namespace SupermanShop.Controllers
             }
             Session["cart"] = cart;
             return Redirect("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
     }
 }
